49703520be42cdef09e8982fc499c795585f9659
Pedro Nascimento <pnascimento@gmail.com>
2011-08-26 21:51:01 -0300

    initial app

---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+
+//
+// START NODE KNOCKOUT TRACKING CODE
+//
+
+    //
+    // Remark: This code MUST be required ONCE somewhere in your application for,
+    // node knockout tracking to work.
+    //
+    require('./vendor/nko')('lyXvQ3xB5YL3dZv0', function(err, res) {
+      if (err) {
+        console.error('Error contacting Node Knockout servers:');
+        console.error(err.stack);
+      }
+    });
+
+//
+//  END NODE KNOCKOUT TRACKING CODE
+//
+
+
+//
+// Start up an http server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>I\'m gonna node knock you out.</h1>');
+}).listen(8080);
